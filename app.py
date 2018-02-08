c6195b7ec1d8229d2daf80596d3f7c748236569c
Hendrikto <hendrik.to@gmail.com>
2018-02-08 09:31:02 +0100

    Implement python app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+visits = 0
+
+
+@app.route("/")
+def hello():
+    global visits
+    visits += 1
+    html = """<h3>Hello {name}!</h3>
+           <b>Hostname:</b> {hostname}<br/>
+           <b>Visits:</b> {visits}"""
+    return html.format(
+        name=os.getenv("NAME", "world"),
+        hostname=socket.gethostname(),
+        visits=visits
+    )
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
